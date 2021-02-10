7df2a810cfca0dc17a0e9c6ae006ca4d344edf43
NIMING <924150590@qq.com>
2021-02-10 19:38:42 +0800

    commit

---
+++ LeetCode/src/daily/最大子序和/Solution.java
@@ -0,0 +1,17 @@
+package 最大子序和;
+
+class Solution {
+//    public int maxSubArray(int[] nums) {
+//        int n = nums.length;
+//        if(n == 0) return 0;
+//        int[] dp = new int[n];
+//        dp[0] = nums[0];
+//        int res = dp[0];
+//        for(int i = 1; i < n; ++i){
+//            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+//            res = Math.max(res, dp[i - 1]);
+//        }
+//        return Math.max(res, dp[n-1]);
+//    }
+
+}
